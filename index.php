3d284ee3774d16627e956425e7ec8ac628f8d1b4
Mahbub Ansary <mdmahbub903@gmail.com>
2018-10-07 20:27:05 +0600

    quick start pack

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Quick Start Pack</title>
+	<!-- Font-Awesome -->
+	<link rel="stylesheet" href="public/css/font-awesome.min.css">
+	<!-- Animate CSS -->
+	<link rel="stylesheet" href="public/css/animate.css" />
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="public/css/bootstrap.min.css" />
+	<!--  Main stylesheet -->
+	<link rel="stylesheet" href="public/css/style.css" />
+	<!--  Responsive styesheet -->
+	<link rel="stylesheet" href="public/css/responsive.css" />
+</head>
+<body>
+	 <!-- =========== gotop end =========== -->
+	<h2>hello world</h2>
+	
+	
+	<!-- Jquery v1.11.3 -->
+	<script src="public/js/jquery.min.js"></script>
+	<!-- Bootstrap Js -->
+	<script  src="<public/js/bootstrap.min.js"  ></script>
+	<!-- Custom JS -->
+	<script  src="public/js/custom.js" ></script>
+</body>
+</html>
